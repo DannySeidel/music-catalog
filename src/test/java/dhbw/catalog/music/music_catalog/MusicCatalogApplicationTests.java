c593bd7934b16ec107d90d8e40c25a0663217dcc
Danny Seidel <danny.seidel02@web.de>
2022-04-28 13:54:56 +0200

    added project files

---
+++ src/test/java/dhbw/catalog/music/music_catalog/MusicCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package dhbw.catalog.music.music_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
